b53ad2e751fb85cc1bec2d1076f81e3cb995f497
Amy <77540881+AmySofia1451@users.noreply.github.com>
2024-09-05 21:39:15 -0400

    First Java studio commit

---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -0,0 +1,27 @@
+package org.launchcode;
+
+import java.util.Scanner;
+
+public class Area {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        double radius;
+
+        do {
+            System.out.println("What is the radius? ");
+        }
+        while (!input.hasNextDouble()) {
+            System.out.println("Sorry, the radius must be a positive number!!!!!!!!!!");
+            input.next();
+        }
+        radius = input.nextDouble();
+    } while (radius< 0);
+
+    double area = Circle.getArea(radius);
+        System.out.println("The area of the circle is "+area);
+        input.close();
+}
+
