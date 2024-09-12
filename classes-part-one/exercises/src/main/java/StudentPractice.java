ec5f4e8d1b52968161176a91d1c575c8b7c1996c
Amy <77540881+AmySofia1451@users.noreply.github.com>
2024-09-11 21:07:42 -0400

    Java classes exercise commit 1

--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,5 +1,6 @@
 public class StudentPractice {
     public static void main(String[] args){
-        //insantiate your Student class below
+        //instantiate your Student class below
+        Student student1 = new Student("Kate", 7777, 1, 4.0);
     }
 }
