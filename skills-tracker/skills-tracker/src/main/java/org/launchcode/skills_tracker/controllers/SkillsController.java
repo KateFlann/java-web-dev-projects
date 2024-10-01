b64f37a5384b1d8d6f7977e99e292a003510fb8e
Amy <77540881+AmySofia1451@users.noreply.github.com>
2024-09-30 21:34:46 -0400

    Studio on controllers in Springboot 2

--- skills-tracker/skills-tracker/src/main/java/org/launchcode/skills_tracker/controllers/SkillsController.java
+++ skills-tracker/skills-tracker/src/main/java/org/launchcode/skills_tracker/controllers/SkillsController.java
@@ -3,48 +3,86 @@ package org.launchcode.skills_tracker.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @ResponseBody
 public class SkillsController {
-
-//    You need to have an h1 with the title “Skills Tracker”,
-//    an h2, and an ol containing three programming languages
-//    of your choosing.
-
-    public String helloForm() {
-
-
+    //    @GetMapping("/")
+//    @ResponseBody
+//    public String hello(){
+//        return "<html>" +
+//                "<body>" +
+//                "<h1>Skills Tracker</h1>" +
+//                "<h2>We Learning!</h2>" +
+//                "<ol>" +
+//                "<li>Java</li>" +
+//                "<li>JavaScript</li>" +
+//                "<li>Python</li>" +
+//                "</body>" +
+//                "</html>";
+//    }
+    @GetMapping("/form")
+    public String form(){
         String html =
                 "<html>" +
                         "<body>" +
-                        "<h1>" +
-                        "Skills Tracker" +
-                        "</h1>" +
-                        "<ol>" +
-                        "<li>" +
-                        "" +
-                        "</li>" +
-                        "<li>" +
-                        "" +
-                        "</li>" +
-                        "<li>" +
-                        "" +
-                        "</li>" +
-                        "</ol>" +
+                        "<form method = 'post' action = '/hello'>" +
+                        "<label for='name'>Name</label><br>" +
+                        "<input type='text' id='name' name='name'><br>" +
+                        "<label for='firstChoice'>My favorite language:</label><br>" +
+                        "<select name='firstChoice' id='firstChoice'>" +
+                        "<option value='Java'>Java</option>" +
+                        "<option value='JavaScript'>JavaScript</option>" +
+                        "<option value='Python'>Python</option>" +
+                        "</select><br>" +
+                        "<label for='secondChoice'>My second favorite language:</label><br>" +
+                        "<select name='secondChoice' id='secondChoice'>" +
+                        "<option value='Java'>Java</option>" +
+                        "<option value='JavaScript'>JavaScript</option>" +
+                        "<option value='Python'>Python</option>" +
+                        "</select><br>" +
+                        "<label for='thirdChoice'>My third favorite language:</label><br>" +
+                        "<select name='thirdChoice' id='thirdChoice'>" +
+                        "<option value='Java'>Java</option>" +
+                        "<option value='JavaScript'>JavaScript</option>" +
+                        "<option value='Python'>Python</option>" +
+                        "</select><br>" +
+                        "<input type = 'submit' value = 'Rank Me!' />" +
+                        "</form>" +
                         "</body>" +
                         "</html>";
         return html;
-
-        @GetMapping("form")
-        public String helloForm(){
-            return "<html>" +
-                    "<body>" +
-                    "<form action='hello' method='post'>" + //submits a request to /hello
-                    "<input type='text' name='name'>" +
-                    "<input type='submit' value='Greet Me!'>" +
-                    "</form>" +
-                    "</body>" +
-                    "</html>";
-        }
+    }
+    //    @GetMapping("/hello")
+    @RequestMapping(value="hello", method = {RequestMethod.GET, RequestMethod.POST})
+    public String helloWithQueryParam(@RequestParam String name, String firstChoice, String secondChoice, String thirdChoice ) {
+        return "<html>" +
+                "<body>" +
+                "<h1>" + name + "</h1>" +
+                "<ol>" +
+                "<li>" +
+                firstChoice +
+                "</li>" +
+                "<li>" +
+                secondChoice +
+                "</li>" +
+                "<li>" +
+                thirdChoice +
+                "</li>" +
+                "</body>" +
+                "</html>";
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
